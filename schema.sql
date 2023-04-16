90c9cd512404391158500f9fa95e918a4da46866
wangyf99 <54357570+wangyf99@users.noreply.github.com>
2023-04-16 14:54:23 -0400

    add frame for database and populate database

--- schema.sql
+++ schema.sql
@@ -1,1 +1,24 @@
--- Write your tables' schemas in this file
+
+CREATE TABLE IF NOT EXISTS uber_data
+(
+    id INTEGER PRIMARY KEY,
+    fare_amount FLOAT64,
+    passenger_count INTEGER,
+    pickup_datetime DATETIME,
+    pickup_latitude FLOAT64,
+    pickup_longitude FLOAT64,
+    dropoff_latitude FLOAT64,
+    dropoff_longitude FLOAT64
+);
+
+CREATE TABLE IF NOT EXISTS taxi_data
+(
+    id INTEGER PRIMARY KEY,
+    fare_amount FLOAT64,
+    passenger_count INTEGER,
+    pickup_datetime DATETIME,
+    pickup_latitude FLOAT64,
+    pickup_longitude FLOAT64,
+    dropoff_latitude FLOAT64,
+    dropoff_longitude FLOAT64
+);
